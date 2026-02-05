44ce1b67030cbdd580d6098d121d31fe5a185660
feran1205 <wendyfaloore@gmail.com>
2026-02-05 15:29:58 +0000

    My note app first migration and template file

--- note_app/urls.py
+++ note_app/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import note_list
 
-urlpatterns = []
+urlpatterns = [ 
+    path('',note_list, name='note_list'),
+]
--- note_app/views.py
+++ note_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Note
 # Create your views here.
+
+
+def note_list(request):
+    notes = Note.objects.all().order_by('-created_at')
+    return render(request, 'note_app/note_list.html', {'notes': notes})
+
