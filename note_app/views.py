54117e47d9ffcc4d155c5bd7af3d4cbffe0905ab
feran1205 <wendyfaloore@gmail.com>
2026-02-19 14:34:31 +0000

    new update

--- note_app/views.py
+++ note_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
 # Create your views here.
 
@@ -7,3 +7,31 @@ def note_list(request):
     notes = Note.objects.all().order_by('-created_at')
     return render(request, 'note_app/note_list.html', {'notes': notes})
 
+def note_detail(request, pk):
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        return render(request, 'note_app/note_not_found.html')
+    return render(request, 'note_app/note_detail.html', {'note': note})
+
+def create_note(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        note = Note.objects.create(title=title, content=content)
+        return redirect('note_list')
+    return render(request, 'note_app/create_note.html')
+
+def edit_note(request, pk):
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        return render(request, 'note_app/note_not_found.html')
+    if request.method == 'POST':
+        note.title = request.POST.get('title')
+        note.content = request.POST.get('content')
+        note.save()
+        # return redirect('note_detail', pk=note.pk)
+        return redirect('note_list')
+    return render(request, 'note_app/edit_note.html', {'note': note})
+        
