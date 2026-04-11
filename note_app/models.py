100cb714dd38d9ca80b1510ec7c668c4752bfb43
Yewande Faloore <wendyfaloore@gmail.com>
2026-04-12 00:15:06 +0100

    My new update

--- note_app/models.py
+++ note_app/models.py
@@ -7,6 +7,6 @@ class Note(models.Model):
     content =models.TextField()
     created_at =models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-     
+    # user = models.ForeignKey(user, on_delete=models.CASCADE, related_name='notes')
     def __str__(self):
        return self.title
